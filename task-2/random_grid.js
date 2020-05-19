48ff6b13d10970e328a26c7216aad748626a2733
Ozukyo <ozukyo@gmail.com>
2020-05-19 21:48:08 +0200

    Add 2 exercise

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,5 +1,32 @@
-function main() {
-    // event handlers and other code here
+function fillGridWithCells() {
+    let container = document.querySelector(".container");
+    const cell = function () {
+        let c = document.createElement("div");
+        c.setAttribute("class", "cell");
+        return c;
+    }
+    for (let i = 0; i < 10 * 10; i++) {
+        container.appendChild(cell());
+    }
 }
 
-main();
+function activateChangingColors() {
+    console.log("active changing");
+    let cellList = document.getElementsByClassName("cell");
+    let randomCell = cellList[Math.floor(Math.random() * cellList.length)];
+    switchColorOfCell(randomCell);
+}
+
+function switchColorOfCell(cell) {
+    if(cell.className === "cell") {
+        cell.setAttribute("class", "cell active");
+    }
+    else
+    {
+        cell.setAttribute("class" ,"cell");
+    }
+}
+
+fillGridWithCells();
+// activateChangingColors();
+setInterval(activateChangingColors, 500);
